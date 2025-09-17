d34b1f830914bd483c3ed2e94e0a119cc66d5809
DannaCZ2007 <dannacz@oru.edu>
2025-09-17 09:19:38 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,12 @@
+let myName = "Danna";
+let title = "Student";
+let experience = "Science fair projects, Event leadership, Forum Talker";
+let education = "Oral Roberts University";
+let skills = "Hard-working, great at mathematics, problem solving"
+//let skills = "something"; //TODO
+//Connect Variables to the HTML (ligaments)
+document. getElementById ("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document. getElementById("education").textContent = education;
+document.getElementById("skills").textContent = skills;
