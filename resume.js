e7822fe9be44398342e41e05dcd4c0fd1a5db1c5
DannaCZ2007 <dannacz@oru.edu>
2025-09-23 14:15:06 -0500

    resume.js

--- resume.js
+++ resume.js
@@ -2,19 +2,21 @@ let myName = "Danna";
 let title = "Student";
 let experience = ["Science fair projects", "Event leadership", "Forum Talker"];
 let education = "Oral Roberts University";
-let skills = ["Hard-working", "great at mathematics", "problem solving"];
-letskillsList =document.createElement("ul");
+let skills = ["Hard-working", "Great at mathematics", "Problem solving"];
+let skillsList = document.createElement("ul");
 
-for (let i= 0; i < skills.length; i++) {
+for (let i= 0; i < skills.length; i++){
   let li = document.createElement("li");
   li.textContent = skills[i];
   skillsList.appendChild(li);
 }
 document.body.appendChild(skillsList);
-//let skills = "something"; //TODO
-//Connect Variables to the HTML (ligaments)
-document.getElementById ("my-name").textContent = myName;
+document.getElementById("my-name").textContent = myName;
 document.getElementById("title").textContent = title;
 document.getElementById("experience").textContent = experience;
 document.getElementById("education").textContent = education;
 document.getElementById("skills").textContent = skills;
+
+function showFunFact(){
+  alert("Fun Fact: I like to dance");
+}
